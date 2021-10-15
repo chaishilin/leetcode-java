f35170e1b3da8c474c8f2414ad9a069a045633e2
chaishilin <592654362@qq.com>
2021-10-15 13:06:51 +0800

    改用StringBuffer

--- 数组/38.外观数列.java
+++ 数组/38.外观数列.java
@@ -15,7 +15,7 @@ class Solution {
         if (n == 1) {
             return "1";
         }
-        String result = "";
+        StringBuffer result = new StringBuffer();
         String resultPre = countAndSay(n - 1);
         resultPre += "a";//哨兵字符，这样的话result就会完整处理完'a'之前的所有字符
         int count = 0; 
@@ -23,12 +23,11 @@ class Solution {
             if (i == 0 ||(resultPre.charAt(i) == resultPre.charAt(i - 1))) {
                 count++;
             } else {
-                result = result + "" + count;
-                result = result + "" + resultPre.charAt(i - 1);
+                result.append(count).append(resultPre.charAt(i - 1));
                 count = 1;
             } 
         }
-        return result;
+        return result.toString();
     }
 }
 // @lc code=end
