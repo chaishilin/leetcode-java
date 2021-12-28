0d71791374db9eede858b50cb5ea6a9d0116a772
chaishilin <592654362@qq.com>
2021-12-28 21:56:21 +0800

    区间重叠最大值问题

---
+++ 数组/1109.航班预订统计.java
@@ -0,0 +1,29 @@
+package 数组;
+/*
+ * @lc app=leetcode.cn id=1109 lang=java
+ *
+ * [1109] 航班预订统计
+ */
+
+// @lc code=start
+class Solution {
+    public int[] corpFlightBookings(int[][] bookings, int n) {
+        //差分-求前缀和方法
+        //对于此类求区间重叠，最大值题，都采用以上通用解法
+
+        int[] result = new int[n];
+        int[] helpList = new int[n+1];//初始化为0
+        int tempSum = 0;
+        for(int i = 0; i < bookings.length;i++){
+            helpList[bookings[i][0]-1] += bookings[i][2];//在进入区间时，加
+            helpList[bookings[i][1]] -= bookings[i][2];//在离开区间后，减
+        }
+        for(int i = 0 ; i < n;i++){
+            tempSum += helpList[i];//求helpList数组的前缀和，即为答案
+            result[i] = tempSum;
+        }
+        return result;
+    }
+}
+// @lc code=end
+
