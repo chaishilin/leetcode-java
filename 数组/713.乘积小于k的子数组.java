42552d9d9eeab6958268378d1aec922878e18769
chaishilin <592654362@qq.com>
2022-05-05 16:08:33 +0800

    学习一个官方题解 O（n）

--- 数组/713.乘积小于k的子数组.java
+++ 数组/713.乘积小于k的子数组.java
@@ -9,35 +9,17 @@ import java.math.BigDecimal;
 
 class Solution {
     public int numSubarrayProductLessThanK(int[] nums, int k) {
-        int[] firstLine = new int[nums.length];//把一个矩阵压缩成了两行，比较费解
-        int[] secondLine = new int[nums.length];
-        int[] temp = firstLine;
-        int result = 0;
-        int curr;
-        for (int i = 0; i < nums.length; i++) {
-            int countOfMax = 0;
-            for (int j = i; j < nums.length; j++) {
-                if (i == 0) {
-                    curr = nums[j];
-                } else {
-                    curr = firstLine[j - 1] * nums[j];//就是类似杨辉三角
-                }
-                if (curr >= k || curr < 0) {
-                    countOfMax += 1;
-                    secondLine[j] = -1;
-                } else {
-                    secondLine[j] = curr;
-                    result += 1;
-                }
+        int n = nums.length, ret = 0;
+        int prod = 1, i = 0;//双指针，i在前面
+        for (int j = 0; j < n; j++) {
+            prod *= nums[j];
+            while (i <= j && prod >= k) {//一直往前走，使得乘积小于k
+                prod /= nums[i];
+                i++;
             }
-            if(countOfMax >= nums.length - i){
-                break;//如果有一行的乘积全部超出了规定，则再有更大的子数组的时候肯定会超规定，因此直接break
-            }
-            temp = firstLine;//交换指针，不是交换数组。
-            firstLine = secondLine;
-            secondLine = temp;
+            ret += j - i + 1;//i和j之间的子数组成绩是满足要求的
         }
-        return result;
+        return ret;
     }
 }
 // @lc code=end
